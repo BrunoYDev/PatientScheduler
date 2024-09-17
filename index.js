c5b7d16079b27a06f58795addbfc8b76425df0f7
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-09-17 15:21:57 -0300

    Add: Dashboard View

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req,res) => {
-    res.send("index");
+    res.render("index");
 });
 
 app.get("/register", (req, res) => {
