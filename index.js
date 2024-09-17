0fc6a9b753053a625b6332c31b74defe6b8057f5
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-09-17 14:42:09 -0300

    Add: Create Page, bootstrap, full calendar, jquery and jquery mask

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 
-app.set("view engine", "ejs");
-mongoose.connect("mongodb://localhost:21017/patientscheduler");
+app.set("view engine", "ejs"); // change view engine to ejs
+mongoose.connect("mongodb://localhost:21017/patientscheduler"); // Create mongoDB connection
 
-app.use(express.static("public"));
+app.use(express.static("public")); // use static files on folder public
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.get("/", (req,res) => {
-    app.render("index");
-});
+// app.get("/", (req,res) => {
+//     res.render("index");
+// });
+
+app.get("/register", (req,res) => {
+    res.render('create');
+})
 
 app.listen(3000, () => {
     console.log("Server running on: http://localhost:3000");
