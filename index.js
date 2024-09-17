61e07b8d883c73bb2f22b2908cf4430e4acd6364
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-09-17 14:10:01 -0300

    Add: DB connection

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:21017/patientscheduler");
 
+app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
@@ -11,5 +14,5 @@ app.get("/", (req,res) => {
 });
 
 app.listen(3000, () => {
-    console.log("Server running on: http://localhost:3000")
+    console.log("Server running on: http://localhost:3000");
 })
